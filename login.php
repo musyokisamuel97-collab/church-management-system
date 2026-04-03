bb6da8e75a2e2729ca3e9486031c3a2b9eb1c45c
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 12:27:07 +0300

    Add user login functionality in login.php
    Implement basic user login functionality with MySQL.

--- login.php
+++ login.php
@@ -1,1 +1,24 @@
+<?php
 
+$conn = new mysqli("localhost", "root", "", "church_db");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "Login successful";
+} else {
+    echo "Invalid username or password";
+}
+
+$conn->close();
+
+?>
