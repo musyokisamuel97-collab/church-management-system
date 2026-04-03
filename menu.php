a970141acedb5c670ed5089a4153b8532333f741
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 15:43:15 +0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,12 @@
+<div class="menu">
+
+<a href="dashboard.php">Dashboard</a>
+<a href="members.php">Members</a>
+<a href="visitors.php">Visitors</a>
+<a href="followups.php">Followups</a>
+<a href="inventory.php">Inventory</a>
+<a href="reports.php">Reports</a>
+<a href="users.php">Users</a>
+<a href="auth/logout.php">Logout</a>
+
+</div>
