595e09c3bc31af69c7e6c155649ae11cafce8532
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 16:16:33 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,15 @@
 <?php include("auth/session_timeout.php"); ?>
+
 <link rel="stylesheet" href="style.css">
 
 <div class="header">
 
 <img src="assets/logo.png" class="logo">
 
-<h2>
-JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI  
+<h3>
+JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI
 <br>
-<span class="subtitle">CITY OF CHAMPIONS</span>
-</h2>
+CITY OF CHAMPIONS
+</h3>
 
 </div>
