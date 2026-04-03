d3fa11cfb9a457a0607ce7d694726de53facdb94
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 13:48:37 +0300

    Create visitors.php for displaying visitor information

---
+++ visitors.php
@@ -0,0 +1,35 @@
+<?php include 'db.php'; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Visitors</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+<h2>Visitors</h2>
+</header>
+
+<div class="container">
+
+<a href="add_visitor.php"><button>Add Visitor</button></a>
+
+<table>
+
+<tr>
+<th>Name</th>
+<th>Phone</th>
+<th>Place of Interest</th>
+<th>Date</th>
+</tr>
+
+</table>
+
+</div>
+
+</body>
+</html>
