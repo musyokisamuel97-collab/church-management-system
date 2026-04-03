a0b37f6ab80c3dfbb75b1c553e67514eb25ee46f
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 13:51:05 +0300

    Create inventory.php

---
+++ inventory.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Church Inventory</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+<h2>Church Inventory</h2>
+</header>
+
+<div class="container">
+
+<a href="add_item.php"><button>Add Item</button></a>
+
+<table>
+
+<tr>
+<th>Item Name</th>
+<th>Department</th>
+<th>Quantity</th>
+<th>Status</th>
+</tr>
+
+</table>
+
+</div>
+
+</body>
+</html>
