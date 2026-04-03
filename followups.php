a87c2c2a50b5a45ec7179f1eb53cb2a83d08f66e
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 13:50:27 +0300

    Add followups.php for follow-up records display

---
+++ followups.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Follow Ups</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+<h2>Follow Ups</h2>
+</header>
+
+<div class="container">
+
+<p>Follow up records will appear here.</p>
+
+</div>
+
+</body>
+</html>
