2ea9ae343918170cb25c2f04a4c0815d19447344
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 16:16:59 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,12 +1,21 @@
 <div class="menu">
 
 <a href="dashboard.php">Dashboard</a>
-<a href="members.php">Members</a>
-<a href="visitors.php">Visitors</a>
-<a href="followups.php">Followups</a>
-<a href="inventory.php">Inventory</a>
-<a href="reports.php">Reports</a>
-<a href="users.php">Users</a>
+
+<a href="members/view_members.php">Members</a>
+
+<a href="visitors/view_visitors.php">Visitors</a>
+
+<a href="followups/followup.php">Followups</a>
+
+<a href="inventory/view_items.php">Inventory</a>
+
+<a href="attendance/mark_attendance.php">Attendance</a>
+
+<a href="users/manage_users.php">Users</a>
+
+<a href="reports/monthly_report.php">Reports</a>
+
 <a href="auth/logout.php">Logout</a>
 
 </div>
