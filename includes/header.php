981658832642b984d48772cb4067ba339e9ee5f5
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 17:18:32 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,57 +1,45 @@
 <?php
 session_start();
-include("config/db.php");
 
-if(isset($_POST['login'])){
-
-$username=$_POST['username'];
-$password=md5($_POST['password']);
-
-$sql="SELECT * FROM users WHERE username='$username' AND password='$password'";
-$result=$conn->query($sql);
-
-if($result->num_rows>0){
-
-$row=$result->fetch_assoc();
-
-$_SESSION['user']=$row['username'];
-$_SESSION['role']=$row['role'];
-
-header("Location: dashboard.php");
-
-}else{
-$error="Invalid login";
-}
+if(!isset($_SESSION['user'])){
+header("Location: ../login.php");
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-<title>Church Login</title>
-<link rel="stylesheet" href="assets/style.css">
-</head>
+<link rel="stylesheet" href="../assets/style.css">
 
-<body class="login-body">
+<header>
 
-<div class="login-box">
+<img src="../assets/logo.png" class="logo">
+
+<div>
 
 <h2>JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI</h2>
+
 <p>CITY OF CHAMPIONS</p>
-
-<form method="post">
-
-<input type="text" name="username" placeholder="Username" required>
-
-<input type="password" name="password" placeholder="Password" required>
-
-<button name="login">Login</button>
-
-</form>
-
-<?php if(isset($error)) echo $error; ?>
 
 </div>
 
-</body>
-</html>
+</header>
+
+<nav>
+
+<a href="../dashboard.php">Dashboard</a>
+
+<a href="../members/members.php">Members</a>
+
+<a href="../visitors/visitors.php">Visitors</a>
+
+<a href="../attendance/attendance.php">Attendance</a>
+
+<a href="../finance/tithes.php">Finance</a>
+
+<a href="../inventory/inventory.php">Inventory</a>
+
+<a href="../reports/reports.php">Reports</a>
+
+<a href="../users/users.php">Users</a>
+
+<a href="../logout.php">Logout</a>
+
+</nav>
