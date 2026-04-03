2531e199fd3ce329897c45a36ae64cb9f8a4f6da
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 13:51:41 +0300

    Add users.php for user management interface

---
+++ users.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Users</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+<h2>User Management</h2>
+</header>
+
+<div class="container">
+
+<p>Admin can manage users here.</p>
+
+</div>
+
+</body>
+</html>
