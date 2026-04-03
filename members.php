26a1b1a655354a9255ca083ec3c5302edeaf3532
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 13:48:01 +0300

    Create members.php for displaying member information

---
+++ members.php
@@ -0,0 +1,35 @@
+<?php include 'db.php'; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Members</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+<h2>Members</h2>
+</header>
+
+<div class="container">
+
+<a href="add_member.php"><button>Add Member</button></a>
+
+<table>
+
+<tr>
+<th>Name</th>
+<th>Phone</th>
+<th>Email</th>
+<th>Address</th>
+</tr>
+
+</table>
+
+</div>
+
+</body>
+</html>
