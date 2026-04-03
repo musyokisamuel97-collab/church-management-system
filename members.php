f5a26aa1939003cf22de9b96414afb33396b636c
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 15:42:14 +0300

    Update members.php

--- members.php
+++ members.php
@@ -1,64 +1,10 @@
-<?php
-include 'db.php';
-
-if(isset($_POST['add'])){
-
-$name=$_POST['name'];
-$phone=$_POST['phone'];
-
-mysqli_query($conn,"INSERT INTO members(name,phone) VALUES('$name','$phone')");
-}
-?>
-
-<header>
-
-<img src="assets/logo.png">
-
-<h2>JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI</h2>
-
-</header>
-
-<div class="main">
-
-<h2>Members</h2>
-
 <form method="POST">
+<input type="text" name="name" placeholder="Full Name" required>
+<input type="date" name="date_joined">
+<input type="text" name="gender" placeholder="Gender">
+<input type="text" name="status" placeholder="Status">
+<input type="text" name="phone" placeholder="Phone Number">
+<input type="text" name="department" placeholder="Department Serving">
 
-<input type="text" name="name" placeholder="Member Name">
-
-<input type="text" name="phone" placeholder="Phone">
-
-<button class="btn" name="add">Add Member</button>
-
+<button type="submit" name="add">Add Member</button>
 </form>
-
-<table>
-
-<tr>
-
-<th>Name</th>
-<th>Phone</th>
-
-</tr>
-
-<?php
-
-$result=mysqli_query($conn,"SELECT * FROM members");
-
-while($row=mysqli_fetch_assoc($result)){
-
-echo "<tr>
-
-<td>".$row['name']."</td>
-
-<td>".$row['phone']."</td>
-
-</tr>";
-
-}
-
-?>
-
-</table>
-
-</div>
