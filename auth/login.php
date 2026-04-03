d1484cb4ee65c84003c8fe8688b025d2d39d5879
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 16:15:43 +0300

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -7,7 +7,8 @@ if(isset($_POST['login'])){
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-$query=mysqli_query($conn,"SELECT * FROM users WHERE username='$username' AND password='$password'");
+$query=mysqli_query($conn,
+"SELECT * FROM users WHERE username='$username' AND password='$password'");
 
 if(mysqli_num_rows($query)==1){
 
@@ -16,30 +17,27 @@ $_SESSION['LAST_ACTIVITY']=time();
 
 header("Location: ../dashboard.php");
 }
-else{
-$error="Invalid login details";
-}
-
 }
 ?>
 
 <link rel="stylesheet" href="../style.css">
 
-<div class="login-box">
+<div class="login">
 
-<h2>JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI</h2>
+<h2>
+JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI
+</h2>
+
 <p>CITY OF CHAMPIONS</p>
 
 <form method="POST">
 
-<input type="text" name="username" placeholder="Username" required>
+<input type="text" name="username" placeholder="Username">
 
-<input type="password" name="password" placeholder="Password" required>
+<input type="password" name="password" placeholder="Password">
 
 <button name="login">Login</button>
 
 </form>
-
-<?php if(isset($error)) echo $error; ?>
 
 </div>
