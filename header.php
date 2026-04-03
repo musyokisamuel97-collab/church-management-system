563758fade8ee177610bd8d128c6fc6e6452c908
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 15:42:38 +0300

    Add header.php with site logo and title

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php include("auth/session_timeout.php"); ?>
+<link rel="stylesheet" href="style.css">
+
+<div class="header">
+
+<img src="assets/logo.png" class="logo">
+
+<h2>
+JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI  
+<br>
+<span class="subtitle">CITY OF CHAMPIONS</span>
+</h2>
+
+</div>
