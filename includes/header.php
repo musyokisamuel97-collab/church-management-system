f9cfbb2cbd5c7f3b70a15db11d9b25a538d29fbb
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 16:45:17 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,11 @@
+<?php include("auth.php"); ?>
 
+<div class="header">
+
+<img src="../assets/logo.png" class="logo">
+
+<h2>JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI - CITY OF CHAMPIONS</h2>
+
+<a href="../includes/logout.php">Logout</a>
+
+</div>
