ccfeeaebfa6e2292f44483c995275478b7f69df8
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 16:15:00 +0300

    Update session_timeout.php

--- auth/session_timeout.php
+++ auth/session_timeout.php
@@ -1,1 +1,21 @@
+<?php
+session_start();
 
+$timeout=300;
+
+if(isset($_SESSION['LAST_ACTIVITY']) &&
+(time()-$_SESSION['LAST_ACTIVITY'])>$timeout){
+
+session_unset();
+session_destroy();
+header("Location: login.php");
+exit();
+}
+
+$_SESSION['LAST_ACTIVITY']=time();
+
+if(!isset($_SESSION['user'])){
+header("Location: login.php");
+exit();
+}
+?>
