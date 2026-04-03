00140017fd5d9bb99403d274f6e6f7bbd90f5d00
musyokisamuel97-collab <musyokisamuel97@gmail.com>
2026-04-03 13:46:24 +0300

    Modify login form fields in login.php
    Updated the login form to remove required attributes for username and password fields.

--- login.php
+++ login.php
@@ -1,24 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 <title>JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI</title>
+<link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
 
-<h2>Church Management System Login</h2>
+<header>
+<h2>JOY CHRISTIAN FELLOWSHIP ONGATA RONGAI</h2>
+</header>
+
+<div class="container">
+
+<h3>Login</h3>
 
 <form action="dashboard.php" method="POST">
 
 <label>Username</label><br>
-<input type="text" name="username" required><br><br>
+<input type="text" name="username"><br><br>
 
 <label>Password</label><br>
-<input type="password" name="password" required><br><br>
+<input type="password" name="password"><br><br>
 
 <button type="submit">Login</button>
 
 </form>
+
+</div>
 
 </body>
 </html>
